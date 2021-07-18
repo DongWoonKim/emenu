5d23f5085c8a219caead425705e84d4d15234000
DongwoonKim <01030985843z@naver.com>
2021-07-18 15:08:57 +0900

    OAuth2 : Google 로그인 구현

--- src/main/java/com/emenu/comito/web/IndexController.java
+++ src/main/java/com/emenu/comito/web/IndexController.java
@@ -1,5 +1,6 @@
 package com.emenu.comito.web;
 
+import com.emenu.comito.config.auth.dto.SessionUser;
 import com.emenu.comito.service.posts.PostsService;
 import com.emenu.comito.web.dto.PostsResponseDto;
 import lombok.RequiredArgsConstructor;
@@ -8,11 +9,14 @@ import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
 
+import javax.servlet.http.HttpSession;
+
 @RequiredArgsConstructor
 @Controller
 public class IndexController {
 
     private final PostsService postsService;
+    private final HttpSession httpSession;
     /*
         머스테치 스타터 덕분에  컨트롤러에서 문자열을 반환할 때
         앞의 경로와 뒤의 파일 확장자는 자동으로 지정된다.
@@ -24,6 +28,11 @@ public class IndexController {
     @GetMapping("/")
     public String index(Model model) {
         model.addAttribute("posts", postsService.findAllDesc());
+        SessionUser user = (SessionUser) httpSession.getAttribute("user");
+
+        if(user != null) {
+            model.addAttribute("userName", user.getName());
+        }
         return "index";
     }
 
@@ -39,6 +48,5 @@ public class IndexController {
 
         return "posts-update";
     }
-
 
 }
